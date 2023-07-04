5e36e2ebe4fee42a5c1aef1afdf12976cbb7a508
Alba <alba@rincon.me>
2023-07-04 21:24:28 +0200

    Fix modal buttons

--- src/routes/AddDayForm.svelte
+++ src/routes/AddDayForm.svelte
@@ -10,6 +10,8 @@
 		} catch (error) {
 			console.log(`Failed to add ${date}-${temperature}: ${error}`);
 		}
+
+		window.add_day_modal.close();
 	};
 
 	const deleteDay = (date: string) => async () => {
@@ -18,11 +20,13 @@
 		} catch (error) {
 			console.log(`Failed to delete ${date}: ${error}`);
 		}
+
+		window.add_day_modal.close();
 	};
 </script>
 
 <dialog id="add_day_modal" class="modal modal-bottom sm:modal-middle">
-	<form method="dialog" class="modal-box">
+	<div class="modal-box">
 		<input
 			class="input input-bordered w-full mb-2"
 			type="number"
@@ -37,11 +41,11 @@
 		<div class="flex">
 			<button class="btn btn-error" on:click={deleteDay(date)}>Delete</button>
 			<div class="w-full text-right">
-				<button class="btn btn-ghost">Cancel</button>
+				<button class="btn btn-ghost" onclick="add_day_modal.close()">Cancel</button>
 				<button class="btn btn-primary" on:click={addDay(date, temperature)}>Save</button>
 			</div>
 		</div>
-	</form>
+	</div>
 	<form method="dialog" class="modal-backdrop">
 		<button>close</button>
 	</form>
