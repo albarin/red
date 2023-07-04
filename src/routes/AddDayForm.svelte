b68bc9b9950ea0f39bdbe8f9151daed7e5a360c5
Alba <alba@rincon.me>
2023-07-04 20:19:53 +0200

    Refactor add day functions

--- src/routes/AddDayForm.svelte
+++ src/routes/AddDayForm.svelte
@@ -1,30 +1,24 @@
 <script lang="ts">
-	import { format } from '$lib/utils/date';
-	import { DateTime } from 'luxon';
-
 	import { db } from '../stores/db';
 
 	export let date: string;
 	export let temperature: number;
 
-	async function addDay() {
+	const addDay = (date: string, temperature: number) => async () => {
 		try {
-			await db.days.put({
-				date: date,
-				temperature: temperature
-			});
+			await db.days.put({ date, temperature });
 		} catch (error) {
 			console.log(`Failed to add ${date}-${temperature}: ${error}`);
 		}
-	}
+	};
 
-	async function deleteDay() {
+	const deleteDay = (date: string) => async () => {
 		try {
 			await db.days.delete(date);
 		} catch (error) {
 			console.log(`Failed to delete ${date}: ${error}`);
 		}
-	}
+	};
 </script>
 
 <dialog id="add_day_modal" class="modal modal-bottom sm:modal-middle">
@@ -41,10 +35,10 @@
 		<input class="input input-bordered w-full mb-2" type="date" bind:value={date} />
 
 		<div class="flex">
-			<button class="btn btn-error" on:click={deleteDay}>Delete</button>
+			<button class="btn btn-error" on:click={deleteDay(date)}>Delete</button>
 			<div class="w-full text-right">
 				<button class="btn btn-ghost">Cancel</button>
-				<button class="btn btn-primary" on:click={addDay}>Save</button>
+				<button class="btn btn-primary" on:click={addDay(date, temperature)}>Save</button>
 			</div>
 		</div>
 	</form>
