4b4a1818d27178bc4274680c09c3424471efed45
Alba <alba@rincon.me>
2023-07-04 08:42:29 +0200

    Render calendar with days data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,6 @@
 <script>
 	import AddDayForm from './AddDayForm.svelte';
 	import Calendar from './Calendar.svelte';
-
-	import { db } from '../stores/db';
-	import { liveQuery } from 'dexie';
-
-	let days = liveQuery(() => db.days.toArray());
 </script>
 
 <div>
@@ -14,12 +9,4 @@
 	<AddDayForm />
 
 	<button class="btn" onclick="add_day_modal.showModal()">Add day</button>
-
-	<!-- <ul>
-		{#if $days}
-			{#each $days as day}
-				<li>{day.date}, {day.temperature}</li>
-			{/each}
-		{/if}
-	</ul> -->
 </div>
--- src/routes/Calendar.svelte
+++ src/routes/Calendar.svelte
@@ -1,11 +1,27 @@
 <script lang="ts">
 	import { DateTime, Interval } from 'luxon';
+	import { liveQuery } from 'dexie';
+
+	import { arrayToObject } from '$lib/utils';
+	import { db } from '../stores/db';
+
+	const startOfMonth = DateTime.now().startOf('month');
+	const endOfMonth = DateTime.now().endOf('month');
+
+	$: days = liveQuery(async () => {
+		const days = await db.days
+			.where('date')
+			.between(startOfMonth.toFormat('yyyy-MM-dd'), endOfMonth.toFormat('yyyy-MM-dd'))
+			.toArray();
+
+		return arrayToObject(days, 'date');
+	});
 
 	const month = DateTime.now().month;
 
 	const weeks = Interval.fromDateTimes(
-		DateTime.now().startOf('month').startOf('week'),
-		DateTime.now().endOf('month').endOf('week')
+		startOfMonth.startOf('week'),
+		endOfMonth.endOf('week')
 	).splitBy({ weeks: 1 });
 
 	const calendar = weeks.map((week) => week.splitBy({ days: 1 }));
@@ -14,7 +30,12 @@
 <div class="grid grid-cols-7 gap-2 text-center mb-4">
 	{#each calendar as week}
 		{#each week as weekday}
-			<span class:text-base-300={weekday.start?.month !== month}>
+			<span
+				class="badge badge-lg py-5"
+				class:badge-error={$days && $days[weekday.start?.toFormat('yyyy-MM-dd')]}
+				class:px-4={weekday.start?.day < 10}
+				class:text-base-300={weekday.start?.month !== month}
+			>
 				{weekday.start?.day}
 			</span>
 		{/each}
