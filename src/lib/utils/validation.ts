1d5a689c58030d1e28cbe63ad6054cf76496da07
Alba <alba@rincon.me>
2023-07-11 08:15:52 +0200

    Allow saving without temperature

--- src/lib/utils/validation.ts
+++ src/lib/utils/validation.ts
@@ -4,11 +4,11 @@ const countDecimals = (number: number): number => {
 }
 
 export const validateTemperature = (value: string | undefined): string | undefined => {
-  const temperature = value ? parseFloat(value) : 0;
-
-  if (!temperature) {
-    return 'Temperature is required';
+  if (!value) {
+    return;
   }
+
+  const temperature = parseFloat(value);
 
   if (temperature < 30 || temperature > 40) {
     return 'Temperature must be between 30 and 40';
