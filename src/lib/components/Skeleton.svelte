df53688594857570139bcec69ce38c24ce962fc7
Alba <alba@rincon.me>
2023-08-31 08:51:12 +0200

    Fix skeleton on mobile

--- src/lib/components/Skeleton.svelte
+++ src/lib/components/Skeleton.svelte
@@ -1,7 +1,7 @@
 <div class="grid grid-cols-4 gap-4 flex-grow m-4 text-primary">
-  <div class="col-span-3 bg-accent-focus rounded-xl animate-pulse" />
-  <div>
-    <div class="bg-accent-focus rounded-xl animate-pulse h-60 mb-4" />
-    <div class="bg-accent-focus rounded-xl animate-pulse h-60" />
-  </div>
+	<div class="h-96 sm:h-full col-span-4 sm:col-span-3 bg-accent-focus rounded-xl animate-pulse" />
+	<div class="col-span-4 sm:col-span-1">
+		<div class="w-full bg-accent-focus rounded-xl animate-pulse h-56 mb-4" />
+		<div class="bg-accent-focus rounded-xl animate-pulse h-40" />
+	</div>
 </div>
