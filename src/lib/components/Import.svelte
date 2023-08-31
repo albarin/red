aebed5230460c4053912d73a6ba471ce82c66348
Alba <alba@rincon.me>
2023-08-31 17:57:04 +0200

    Show validation and uploading errors on importing

--- src/lib/components/Import.svelte
+++ src/lib/components/Import.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte';
 	import { db } from '../../stores/db';
 
+	const dispatch = createEventDispatcher();
+
 	let uploader: HTMLInputElement;
+	let uploading: boolean = false;
+	let finishedUploading: boolean = false;
+	let error: string = '';
+
+	const setError = (msg) => {
+		console.error(msg);
+		error = msg;
+		uploading = false;
+		finishedUploading = true;
+	};
+
 	const upload = async (e) => {
+		error = '';
+		uploading = true;
+
 		let maxFileSize = 3145728;
 		const file = uploader.files[0];
 
@@ -10,12 +27,12 @@
 		const fileExtension = fileExtensionArray[fileExtensionArray.length - 1];
 
 		if (!fileExtension.includes('csv')) {
-			console.error('Wrong file type');
+			setError('Wrong file type, should be CSV');
 			return;
 		}
 
 		if (file.size > maxFileSize) {
-			console.error('Above the max file size threshold');
+			setError('The file is too big');
 			return;
 		}
 
@@ -23,6 +40,11 @@
 		const rows = fileContent.trim().split('\n');
 
 		const headers = rows[0].split(',');
+		if (headers.length != 5) {
+			setError('Invalid CSV format');
+			return;
+		}
+
 		const data = rows.slice(1).map((row) => {
 			const values = row.split(',');
 			return headers.reduce((obj, header, i) => {
@@ -37,14 +59,36 @@
 		try {
 			await db.days.bulkPut(data);
 		} catch (error) {
-			console.error(`Failed to import ${file.name}: ${error}`);
+			setError(`Failed to import ${file.name}: ${error}`);
+			return;
 		}
+
+		uploading = false;
+		finishedUploading = true;
 	};
 </script>
 
-<input
-	type="file"
-	class="file-input file-input-sm file-input-bordered file-input-primary w-full max-w-xs"
-	bind:this={uploader}
-	on:change|preventDefault={upload}
-/>
+<div class="flex gap-4 items-center">
+	<input
+		type="file"
+		class="file-input file-input-sm file-input-bordered file-input-primary w-full max-w-xs"
+		bind:this={uploader}
+		on:change|preventDefault={upload}
+	/>
+
+	{#if uploading}
+		<span class="loading loading-spinner loading-md" />
+	{/if}
+
+	{#if finishedUploading}
+		{#if error}
+			<span class="text-error">
+				{error}
+			</span>
+		{:else}
+			<span class="text-primary">
+				Import finished go to <a class="btn-link" href="/">calendar</a>!
+			</span>
+		{/if}
+	{/if}
+</div>
